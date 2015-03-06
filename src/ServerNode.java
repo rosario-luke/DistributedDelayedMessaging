8ed851ee5c0b0368e89e832bff711275bd45c641
rosario-luke <lrosari2@illinois.edu>
2015-03-05 20:25:45 -0600

    Finished Implementing Response for All Consistency Models

--- src/ServerNode.java
+++ src/ServerNode.java
@@ -7,7 +7,6 @@ import java.util.concurrent.DelayQueue;
 import java.io.*;
 
 
-
 public class ServerNode extends Thread {
 
     private ConcurrentHashMap<Integer, ServerValue> myTable;
@@ -16,39 +15,39 @@ public class ServerNode extends Thread {
     private MessageGenerator generator;
     private HashMap<Character, DelayedServerMessage> lastMessages;
     private Random rand;
-	public static void main(String[] args) {
+    private DelayQueue<DelayedServerMessage> delayQueue;
+    public static void main(String[] args) {
 
-		try {
-			if(args.length != 1 && args.length != 2){
-				System.out.println("Args should be 1 string containing the configuration file and optional command file");
-				System.exit(1);
+        try {
+            if (args.length != 1 && args.length != 2) {
+                System.out.println("Args should be 1 string containing the configuration file and optional command file");
+                System.exit(1);
 
-			}
+            }
 
-			new ServerNode().startServer(args);
-		} catch (Exception e) {
-			System.out.println("I/O failure: " + e.getMessage());
-			e.printStackTrace();
-		}
+            new ServerNode().startServer(args);
+        } catch (Exception e) {
+            System.out.println("I/O failure: " + e.getMessage());
+            e.printStackTrace();
+        }
 
-	}
+    }
 
-	public void startServer(String[] files) throws Exception {
+    public void startServer(String[] files) throws Exception {
 
-		String file = files[0];
+        String file = files[0];
         String commands = (files.length == 2) ? files[1] : null; // Set command file if specified
         myTable = new ConcurrentHashMap<Integer, ServerValue>();
         myCommands = new ConcurrentHashMap<Command, CommandResponse>();
         lastMessages = new HashMap<Character, DelayedServerMessage>();
-		ServerSocket serverSocket = null;
-		boolean listening = true;
-		config = ConfigurationManager.createConfig(file, commands);
-		
-		try{
-			
-			
-			
-			DelayQueue<DelayedServerMessage> dq = new DelayQueue<DelayedServerMessage>();
+        ServerSocket serverSocket = null;
+        boolean listening = true;
+        config = ConfigurationManager.createConfig(file, commands);
+
+        try {
+
+
+            delayQueue = new DelayQueue<DelayedServerMessage>();
 
 
             Iterator<ServerInfo> it = config.getServerIterator();
@@ -59,112 +58,119 @@ public class ServerNode extends Thread {
             rand = new Random();
             generator = new MessageGenerator(config, rand, lastMessages);
 
-			// Setup CommandConsole and run
-			CommandConsole commandConsole = new CommandConsole(dq, config, "CommandConsole", myTable, myCommands, generator);
-			commandConsole.start();
-			
-			// Setup clientNode and run
-			ClientNode clientNode = new ClientNode(dq, config, "ClientNode");
-			clientNode.start();
-			
-			serverSocket = new ServerSocket(config.getHostPort());
+            // Setup CommandConsole and run
+            CommandConsole commandConsole = new CommandConsole(delayQueue, config, "CommandConsole", myTable, myCommands, generator);
+            commandConsole.start();
+
+            // Setup clientNode and run
+            ClientNode clientNode = new ClientNode(delayQueue, config, "ClientNode");
+            clientNode.start();
+
+            serverSocket = new ServerSocket(config.getHostPort());
             serverSocket.setReuseAddress(true);
-			System.out.println("Server now listening on port" + config.getHostPort());
-			while (listening) {
-				handleServerRequest(serverSocket);
-			}
-	 
+            System.out.println("Server now listening on port" + config.getHostPort());
+            while (listening) {
+                handleServerRequest(serverSocket);
+            }
 
 
-		} catch (IOException e) {
-			System.err.println("Could not listen on port: " + config.getHostPort());
+        } catch (IOException e) {
+            System.err.println("Could not listen on port: " + config.getHostPort());
 
-			System.exit(-1);
-		} finally {
+            System.exit(-1);
+        } finally {
             serverSocket.close();
-			
-		}
 
-	}
-	
-	private void handleServerRequest(ServerSocket serverSocket) {
-		try {
-			Socket socket = serverSocket.accept();
-			BufferedReader _in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        }
+
+    }
+
+    private void handleServerRequest(ServerSocket serverSocket) {
+        try {
+            Socket socket = serverSocket.accept();
+            BufferedReader _in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             String fullMessage = _in.readLine();
-            if(fullMessage.startsWith("Command")){ // WE ARE RECIEVING A COMMAND
+            if (fullMessage.startsWith("Command")) { // WE ARE RECIEVING A COMMAND
                 Command command = new Command(fullMessage.split("::")[0]);
                 long timestamp = Long.parseLong(fullMessage.split("::")[1]);
 
-                    if(command.isLinearOrSequential()){
-                        switch(command.getType()){
-                            case Command.GET_COMMAND:
-                                if(command.getModel() == Command.LINEARIZABLE_MODEL && command.getOrigin() == config.getHostIdentifier()){ // IF THE COMMAND IF FROM MYSELF AND LINEARIZABLE
-                                    System.out.println("get(" + command.getKey() + ") = " + myTable.get(command.getKey()).getValue());
-                                }
-                                break;
-                            case Command.DELETE_COMMAND:
-                                myTable.remove(command.getKey());
-                                System.out.println("Key " + command.getKey() + " deleted");
-                                break;
-                            case Command.INSERT_COMMAND:
-                                myTable.put(command.getKey(), new ServerValue(command.getValue(), timestamp));
-                                System.out.println("Inserted key" + command.getKey());
-                                break;
-                            case Command.UPDATE_COMMAND:
-                                ServerValue sv = myTable.get(command.getKey());
-                                myTable.put(command.getKey(), new ServerValue(command.getValue(), timestamp));
-                                if(sv != null){
-                                    System.out.println("Key " + command.getKey() + " changed from " + sv.getValue() + " to " + command.getValue());
-                                } else {
-                                    System.out.println("Inserted key" + command.getKey());
-                                }
-                                break;
-                        }
-                        if(command.getOrigin() == config.getHostIdentifier()) { // IF THE COMMAND IS FROM MYSELF NOTIFY SO THAT COMMANDS CONTINUE TO EXECUTE
-                            //myCommands.get(command).recieveCommandFromMyself();
-                            CommandResponse cr = myCommands.get(command);
-                            if(cr == null){
-                                System.out.println("cr was null");
-                            } else {
-                                cr.recieveCommandFromMyself();
+                if (command.isLinearOrSequential()) {
+                    switch (command.getType()) {
+                        case Command.GET_COMMAND:
+                            if (command.getModel() == Command.LINEARIZABLE_MODEL && command.getOrigin() == config.getHostIdentifier()) { // IF THE COMMAND IF FROM MYSELF AND LINEARIZABLE
+                                System.out.println("get(" + command.getKey() + ") = " + myTable.get(command.getKey()).getValue());
                             }
-                        }
-                    } else {
-                        DelayedServerMessage m;
-                        switch(command.getType()){
-                            case Command.GET_COMMAND:
-
-                                break;
-                            case Command.DELETE_COMMAND:
-                                myTable.remove(command.getKey());
-                                System.out.println("Key " + command.getKey() + " deleted");
-                                break;
-                            case Command.INSERT_COMMAND:
-                                myTable.put(command.getKey(), new ServerValue(command.getValue(), timestamp));
+                            break;
+                        case Command.DELETE_COMMAND:
+                            myTable.remove(command.getKey());
+                            System.out.println("Key " + command.getKey() + " deleted");
+                            break;
+                        case Command.INSERT_COMMAND:
+                            myTable.put(command.getKey(), new ServerValue(command.getValue(), timestamp));
+                            System.out.println("Inserted key" + command.getKey());
+                            break;
+                        case Command.UPDATE_COMMAND:
+                            ServerValue sv = myTable.get(command.getKey());
+                            myTable.put(command.getKey(), new ServerValue(command.getValue(), timestamp));
+                            if (sv != null) {
+                                System.out.println("Key " + command.getKey() + " changed from " + sv.getValue() + " to " + command.getValue());
+                            } else {
                                 System.out.println("Inserted key" + command.getKey());
-                                break;
-                            case Command.UPDATE_COMMAND:
-                                ServerValue sv = myTable.get(command.getKey());
-                                myTable.put(command.getKey(), new ServerValue(command.getValue(), timestamp));
-                                if(sv != null){
-                                    System.out.println("Key " + command.getKey() + " changed from " + sv.getValue() + " to " + command.getValue());
-                                } else {
-                                    System.out.println("Inserted key" + command.getKey());
-                                }
-                                break;
-                        }
-
+                            }
+                            break;
                     }
+                    if (command.getOrigin() == config.getHostIdentifier()) { // IF THE COMMAND IS FROM MYSELF NOTIFY SO THAT COMMANDS CONTINUE TO EXECUTE
+                        myCommands.get(command).recieveCommandFromMyself();
+                    }
+                } else {
+                    DelayedServerMessage m = null;
+                    ServerValue sv = myTable.get(command.getKey());
+                    switch (command.getType()) {
+                        case Command.GET_COMMAND:
+                            if(sv != null){
+                                m = generator.GenerateResponseMessageFromCommand(command,sv.getValue(),sv.getTimestamp());
+                            } else {
+                                m = generator.GenerateResponseMessageFromCommand(command, Integer.MIN_VALUE, -1);
+                            }
+                            break;
+                        case Command.DELETE_COMMAND:
+                            myTable.remove(command.getKey());
+                            System.out.println("Key " + command.getKey() + " deleted");
+                            m = generator.GenerateResponseMessageFromCommand(command,sv.getValue(),sv.getTimestamp());
+                            break;
+                        case Command.INSERT_COMMAND:
+                            myTable.put(command.getKey(), new ServerValue(command.getValue(), timestamp));
+                            System.out.println("Inserted key" + command.getKey());
+                            m = generator.GenerateResponseMessageFromCommand(command, command.getValue(),timestamp);
+                            break;
+                        case Command.UPDATE_COMMAND:
+                            myTable.put(command.getKey(), new ServerValue(command.getValue(), timestamp));
+                            if (sv != null) {
+                                System.out.println("Key " + command.getKey() + " changed from " + sv.getValue() + " to " + command.getValue());
+                            } else {
+                                System.out.println("Inserted key" + command.getKey());
+                            }
+                            m = generator.GenerateResponseMessageFromCommand(command, command.getValue(),timestamp);
+                            break;
+                    }
+                    delayQueue.add(m);
+
+                }
 
 
-            } else { // WE ARE RECIEVING A RESPONSE/ACKNOWLEDGEMENT
+            } else { // WE ARE RECEIVING A RESPONSE/ACKNOWLEDGEMENT
+
+                Response response = new Response(fullMessage.split("::")[0]);
+                Command command = new Command(fullMessage.split("::")[1]);
+
+                CommandResponse commandRes = myCommands.get(command);
+                commandRes.addResponse(response);
 
             }
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
 
 }
