69cb0d4595ccc25f543b4edd66bd4cb4cebe1a5d
rosario-luke <lrosari2@illinois.edu>
2015-03-07 16:10:02 -0600

    Implemented Delay, Show-All, and Search

--- src/CommandConsole.java
+++ src/CommandConsole.java
@@ -18,7 +18,7 @@ public class CommandConsole implements Runnable {
 
     private ConcurrentHashMap<Command, CommandResponse> myCommands;
 
-    public CommandConsole(DelayQueue<DelayedServerMessage> dq, ConfigurationFile con, String name, ConcurrentHashMap<Integer, ServerValue> map,ConcurrentHashMap<Command, CommandResponse> mC, MessageGenerator g) {
+    public CommandConsole(DelayQueue<DelayedServerMessage> dq, ConfigurationFile con, String name, ConcurrentHashMap<Integer, ServerValue> map, ConcurrentHashMap<Command, CommandResponse> mC, MessageGenerator g) {
         delayQueue = dq;
         config = con;
         generator = g;
@@ -36,86 +36,35 @@ public class CommandConsole implements Runnable {
         _input.nextLine();
 
 
-
-            if (config.hasCommandFile()) {
-                System.out.println("Sending commands from file");
-                File cf = new File(config.getCommandFile());
-                Boolean fileFound = false;
-                try{
-                    _input = new Scanner(cf);
-                    fileFound = true;
-                } catch(FileNotFoundException e){
-                    System.out.println("Unable to find file, skipping to manual input");
-                }
-
-                if(fileFound) {
-                    ArrayList<DelayedServerMessage> mList;
-                    while (_input.hasNext()) {
-                        inputLine = _input.nextLine();
-                        try {
-                            mList = generator.GenerateMessageFromCommandString(inputLine);
-                        } catch(Exception e){
-                            System.out.println("Error occured while generating message, skipping to next command");
-                            continue;
-                        }
-                        if (mList.size() == 1 && mList.get(0).isReadSeq()) {
-                            // IF THE COMMAND IS READ AND MODEL IS SEQUENTIALLY CONSISTENT, DON'T TOTAL BROADCAST, JUST OUTPUT VALUE
-                            Command c = mList.get(0).getCommand();
-                            myCommands.put(c, new CommandResponse(c));
-                            if (myTable.get(c.getKey()) != null) {
-                                System.out.println("get(" + c.getKey() + ") = " + myTable.get(c.getKey()).getValue());
-                            } else {
-                                System.out.println("get(" + c.getKey() + ") occurred but value did not exist");
-                            }
-                        } else {
-                            Command c = mList.get(0).getCommand();
-                            myCommands.put(c, new CommandResponse(c));
-                            for (DelayedServerMessage nMessage : mList) {
-                                delayQueue.add(nMessage);
-                                //System.out.println("Sent '" + nMessage.getCommand().toString() + "' to " + nMessage.getServerInfo().getIdentifier() + ", system time is " + TimeUnit.MILLISECONDS.toSeconds(System.currentTimeMillis()));
-                            }
-                            myCommands.get(mList.get(0).getCommand()).waitForResponses();
-                            printAcknowledgementForCommand(c);
-                        }
-
-                    }
-                }
-                _input.close();
+        if (config.hasCommandFile()) {
+            System.out.println("Sending commands from file");
+            File cf = new File(config.getCommandFile());
+            Boolean fileFound = false;
+            try {
+                _input = new Scanner(cf);
+                fileFound = true;
+            } catch (FileNotFoundException e) {
+                System.out.println("Unable to find file, skipping to manual input");
             }
-            _input = new Scanner(System.in);
-            // Start asking for commands
-            System.out.println("Please enter commands now");
-        ArrayList<DelayedServerMessage> mList;
-            while (!((inputLine = _input.nextLine()).equals("exit"))) {
 
-                try {
-                    mList = generator.GenerateMessageFromCommandString(inputLine);
-                } catch(Exception e){
-                    System.out.println("Error occured while generating message, skipping to next command");
-                    continue;
+            if (fileFound) {
+                while (_input.hasNext()) {
+                    inputLine = _input.nextLine();
+                    handleCommand(inputLine);
+
                 }
-                if(mList.size() == 1 && mList.get(0).isReadSeq()){
-                    // IF THE COMMAND IS READ AND MODEL IS SEQUENTIALLY CONSISTENT, DON'T TOTAL BROADCAST, JUST OUTPUT VALUE
-                    Command c = mList.get(0).getCommand();
-                    if(myTable.get(c.getKey()) != null) {
-                        System.out.println("get(" + c.getKey() + ") = " + myTable.get(c.getKey()).getValue());
-                    } else {
-                        System.out.println("get(" + c.getKey() + ") occurred but value did not exist");
-                    }
-                } else {
-                    Command c = mList.get(0).getCommand();
-                    myCommands.put(c, new CommandResponse(c));
-                    for (DelayedServerMessage nMessage : mList) {
-                        delayQueue.add(nMessage);
-                        System.out.println("Sent '" + nMessage.getCommand().toString() + "' to " + nMessage.getServerInfo().getIdentifier() + ", system time is " + TimeUnit.MILLISECONDS.toSeconds(System.currentTimeMillis()));
-                    }
-                }
-                myCommands.get(mList.get(0).getCommand()).waitForResponses();
-                System.out.println("Command " + mList.get(0).getCommand().toString() + " was acknowledged");
-                System.out.println("Input next command");
             }
+            _input.close(); // Close File Reader
+        }
+        _input = new Scanner(System.in);
+        // Start asking for commands
+        System.out.println("Please enter commands now");
+
+        while (!((inputLine = _input.nextLine()).equals("exit"))) {
+
+            handleCommand(inputLine);
+        }
         _input.close();
-
 
 
     }
@@ -128,17 +77,17 @@ public class CommandConsole implements Runnable {
         t.start();
     }
 
-    public void printAcknowledgementForCommand(Command c){
+    public void printAcknowledgementForCommand(Command c) {
 
-        switch(c.getType()){
+        switch (c.getType()) {
             case Command.GET_COMMAND:
-                if(c.getModel() == Command.LINEARIZABLE_MODEL){
-                    System.out.println("get("+ c.getKey() + ") = " + myTable.get(c.getKey()).getValue());
+                if (c.getModel() == Command.LINEARIZABLE_MODEL) {
+                    System.out.println("get(" + c.getKey() + ") = " + myTable.get(c.getKey()).getValue());
                 } else {
                     Response best = analyzeGetResponses(myCommands.get(c).getResponseList());
                     System.out.println("get(" + c.getKey() + ") = (" + best.getValue() + ", " + best.getTimestamp() + ") accepted");
-                    for(Response r : myCommands.get(c).getResponseList()){
-                        if(!r.equals(best)){
+                    for (Response r : myCommands.get(c).getResponseList()) {
+                        if (!r.equals(best)) {
                             System.out.println("get(" + c.getKey() + ") = (" + r.getValue() + ", " + r.getTimestamp() + ") not accepted");
                         }
                     }
@@ -148,7 +97,7 @@ public class CommandConsole implements Runnable {
                 System.out.println("Inserted key " + c.getKey());
                 break;
             case Command.UPDATE_COMMAND:
-                System.out.println("Key " + c.getKey() + " updated to " +c.getValue());
+                System.out.println("Key " + c.getKey() + " updated to " + c.getValue());
                 break;
             case Command.DELETE_COMMAND:
                 System.out.println("Key " + c.getKey() + " deleted");
@@ -158,10 +107,61 @@ public class CommandConsole implements Runnable {
 
     }
 
-    public Response analyzeGetResponses(ArrayList<Response> responseList){
+    public void handleCommand(String inputLine) {
+
+        if(inputLine.contains("Delay")){
+            int delay = Integer.parseInt(inputLine.split(" ")[1]);
+            try {
+                Thread.sleep(delay * 1000);
+            } catch(InterruptedException e){
+                System.out.println("Error occurred while delaying next command");
+            }
+
+        } else if(inputLine.contains("Search")){
+            int key = Integer.parseInt(inputLine.split(" ")[1]);
+            ServerValue v = myTable.get(key);
+            if(v == null) {
+                System.out.println("Value for " + key + " is not defined");
+            } else {
+                System.out.println("Value (" + key + ") = " + v.getValue());
+            }
+        } else if(inputLine.contains("Show-all")){
+            for(Map.Entry<Integer, ServerValue> ksv : myTable.entrySet()){
+                System.out.println("Value (" + ksv.getKey() + ") = " + ksv.getValue().getValue());
+            }
+        }
+
+        ArrayList<DelayedServerMessage> mList;
+        try {
+            mList = generator.GenerateMessageFromCommandString(inputLine);
+        } catch (Exception e) {
+            System.out.println("Error occured while generating message, skipping to next command");
+            return;
+        }
+        if (mList.size() == 1 && mList.get(0).isReadSeq()) {
+            // IF THE COMMAND IS READ AND MODEL IS SEQUENTIALLY CONSISTENT, DON'T TOTAL BROADCAST, JUST OUTPUT VALUE
+            Command c = mList.get(0).getCommand();
+            if (myTable.get(c.getKey()) != null) {
+                System.out.println("get(" + c.getKey() + ") = " + myTable.get(c.getKey()).getValue());
+            } else {
+                System.out.println("get(" + c.getKey() + ") occurred but value did not exist");
+            }
+        } else {
+            Command c = mList.get(0).getCommand();
+            myCommands.put(c, new CommandResponse(c));
+            for (DelayedServerMessage nMessage : mList) {
+                delayQueue.add(nMessage);
+            }
+        }
+        myCommands.get(mList.get(0).getCommand()).waitForResponses();
+        printAcknowledgementForCommand(mList.get(0).getCommand());
+
+    }
+
+    public Response analyzeGetResponses(ArrayList<Response> responseList) {
         Response bestResponse = responseList.get(0);
-        for(Response r: responseList){
-            if(r.getTimestamp() > bestResponse.getTimestamp()){
+        for (Response r : responseList) {
+            if (r.getTimestamp() > bestResponse.getTimestamp()) {
                 bestResponse = r;
             }
         }
